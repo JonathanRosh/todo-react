565f98bd74acff70b1e3dd27745441f3f70c11b3
Jonathan <yonatanorsh@mail.tau.ac.il>
2025-04-16 20:00:33 +0300

    Add done task functionallity

--- server/src/routes/tasks.ts
+++ server/src/routes/tasks.ts
@@ -43,4 +43,20 @@ router.delete("/:id", async (req: Request<{ id: string }>, res: Response) => {
   }
 });
 
+//Toggle task completion
+router.patch("/:id", async (req: Request<{ id: string }>, res: Response) => {
+  const id = req.params.id;
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    task.completed = !task.completed;
+    const updatedTask = await task.save();
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to update task" });
+  }
+});
+
 export default router;
